0e3eceef70de823a7fbc7d50b46c7f1262b3b609
Cecilia Zanella <zanella.ceciliasoledad@gmail.com>
2024-01-11 10:38:03 -0300

    Generación de la Page Gracias / Ajustes en Formulario y en Form.php

--- form.php
+++ form.php
@@ -6,17 +6,17 @@ $localidad = $_POST["Localidad"];
 $mensaje = $_POST["Escribí tu mensaje..."];
 
 
-$mensaje = "Este mensaje fue enviado por:" . $nombre . "/r/n";
-$mensaje = "Su e-mail es:" . $email . "/r/n";
-$mensaje = "Es de la Localidad de:" . $localidad . "/r/n";
-$mensaje = "Su mensaje es:" . $_POST["Escribí tu mensaje..."] ."/r/n";
-$mensaje = "Enviado el:".date{"d/m/y",time()};
-
-?>
+$mensaje = "Este mensaje fue enviado por: " . $nombre . "/r/n";
+$mensaje = "Su e-mail es: " . $email . "/r/n";
+$mensaje = "Es de la Localidad de: " . $localidad . "/r/n";
+$mensaje = "Su mensaje es: " . $_POST["Escribí tu mensaje..."] ."/r/n";
+$mensaje = "Enviado el: " . date("d/m/y", time());
 
 $para = "corazon.de.chocolate@gmail.com";
-$asunto = "Mail de la Web Corazón de Chocolate";
+$asunto = "Tienes un Mail desde la Web de Corazón de Chocolate";
 
-mail($para, $asunto, utf8_decode(mensaje), $header);
+mail($para, $asunto, utf8_decode($mensaje), $header);
 
-header "location: exito.html"
+header{Location:"./pages/gracias.html"};
+
+?>
