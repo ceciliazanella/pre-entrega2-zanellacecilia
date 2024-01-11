c002814273a1876abc3270d42d47b6c34dc6d201
Cecilia Zanella <zanella.ceciliasoledad@gmail.com>
2024-01-11 08:27:23 -0300

    Ajuste de Animaciones en Tarjetas de Page Tortas Personales y Logo / Modificación de tamaño tipográfico en Footer

--- form.php
+++ form.php
@@ -1,21 +1,21 @@
 <?php
 
-$nombre=$_POST["Nombre"]:
-$email=$_POST["Email"]:
-$localidad=$_POST["Localidad"]:
-$mensaje=$_POST["Escribí tu mensaje..."]:
+$nombre = $_POST["Nombre"];
+$email = $_POST["Email"];
+$localidad = $_POST["Localidad"];
+$mensaje = $_POST["Escribí tu mensaje..."];
 
 
-$mensaje="Este mensaje fue enviado por:" $nombre "./r/n";
-$mensaje="Su e-mail es:" $email "./r/n";
-$mensaje="Es de la Localidad de:" $localidad "./r/n";
-$mensaje="Su mensaje es:"$mensaje "./r/n";
-$mensaje="Enviado el:".date{"d/m/y",time()};
+$mensaje = "Este mensaje fue enviado por:" . $nombre . "/r/n";
+$mensaje = "Su e-mail es:" . $email . "/r/n";
+$mensaje = "Es de la Localidad de:" . $localidad . "/r/n";
+$mensaje = "Su mensaje es:" . $_POST["Escribí tu mensaje..."] ."/r/n";
+$mensaje = "Enviado el:".date{"d/m/y",time()};
 
 ?>
 
-$para="corazondechocolate@gmail.com";
-$asunto="Mail desde la Web de Corazón de Chocolate";
+$para = "corazon.de.chocolate@gmail.com";
+$asunto = "Mail de la Web Corazón de Chocolate";
 
 mail($para, $asunto, utf8_decode(mensaje), $header);
 
